3f4589feb33661abe9b3a9a67950295d05d49c5d
fjonamalo <fjonamalo0@gmail.com>
2023-08-17 11:43:39 +0200

    Passing props to component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
       <input
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
         type="input"
         placeholder="Enter your todo"
         className="input__box"
